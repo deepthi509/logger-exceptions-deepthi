6114e0c10ea8170c572f9fd248753ffa0a758bc2
deepthi509 <68539554+deepthi509@users.noreply.github.com>
2020-07-26 14:03:27 +0530

    Add files via upload

--- module-info.java
+++ module-info.java
@@ -1,45 +1,39 @@
 package house;
 
+  
 
-import java.util.*;
+import java.util.Scanner;
 import java.util.logging.Level; 
 import java.util.logging.Logger; 
 import java.util.logging.*; 
-public class Logger2{
-	static int n;
-	static float cost;
+public class Logger3
+ {
+static double simp_interest,comp_interest;
+static double prin_amount, rate, time;
 private final static Logger LOGGER =  
                 Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
-public void method() {
- Scanner sc=new Scanner(System.in);
+public void calculation() 
+	{
+ 	Scanner scanner=new Scanner (System. in);
+LOGGER.log(Level.INFO, "enter the principle amount"); 
+prin_amount=scanner.nextDouble();
+LOGGER.log(Level.INFO, "enter the time period"); 
+time=scanner.nextDouble();
+LOGGER.log(Level.INFO, "enter the rate of interest"); 
+rate=scanner.nextDouble();
+ 	 simp_interest=(prin_amount*time*rate)/100;
+		LOGGER.log(Level.INFO, "the simple interest is:"+simp_interest); 
+	comp_interest=prin_amount * Math.pow(1.0+rate/100.0,time) - prin_amount;
+		LOGGER.log(Level.INFO, "the compound interest is"+comp_interest); 
+} 
 
-LOGGER.log(Level.INFO, "Enter the square feet:");
-            n=sc.nextInt();
-LOGGER.log(Level.INFO, "Enter the choice:");
-	int choice=sc.nextInt();
+public static void main(String args[]) 
+	{
 
-    switch(choice) {
-	case 1:
-        cost=1200*n;
-    	break;
-    case 2:
-        cost=1500*n;
-    	break;
-    case 3:
-    	cost=1800*n;
-    	break;
-    case 4:
-    	cost=2500*n;
-    	break;
-    default:
-    	LOGGER.log(Level.INFO, "invalid choice");
-    }
-  LOGGER.log(Level.INFO, "cost for house"+cost);
-   }
-public static void main(String args[]) {
+Logger3 interest=new Logger3();
+	interest.calculation();
 
-	    Logger2 hc=new Logger2();
-	hc.method();
-
+	}
 }
-}
+
+
